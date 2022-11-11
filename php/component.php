b2d3c010a1ac2f78a80cc0539f98295a1ffeff5f
Isnaeni Abdan Syakuro <isnaeniabdan113@gmail.com>
2022-11-12 05:34:05 +0700

    Push

---
+++ php/component.php
@@ -0,0 +1,30 @@
+<?php
+
+function component(){
+    $element = "
+    
+    <div class=\"col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat\">
+    <div class=\"featured__item\">
+        <div class=\"featured__item__pic set-bg\" data-setbg=\"img/featured/beras-3.jpg\">
+            <ul class=\"featured__item__pic__hover\">
+                <li>
+                    <a href=\"#\"><i class=\"fa fa-heart\"></i></a>
+                </li>
+                <li>
+                    <a href=\"#\"><i class=\"fa fa-retweet\"></i></a>
+                </li>
+                <li>
+                    <a href=\"#\"><i class=\"fa fa-shopping-cart\"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div class=\"featured__item__text\">
+            <h6><a href=\"#\">Beras Rakyat</a></h6>
+            <h5>Rp.300.000,00</h5>
+        </div>
+    </div>
+</div>
+    
+    ";
+}
+echo $element;
